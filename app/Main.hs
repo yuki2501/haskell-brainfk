b18f9c392f5511932696a9fdc327e57f3a35e1ec
yuki2501 <yuki2501@protonmail.com>
2021-02-06 05:56:59 +0900

    [change] change for using loop.

--- app/Main.hs
+++ app/Main.hs
@@ -5,13 +5,21 @@ import Data.Text
 import Data.List
 import Data.Word
 import Control.Monad.Skeleton
+import System.IO
 main :: IO ()
 main = do
-  bf <- getLine
-  let parsedbf = parseBrainFk $ pack bf
-  runM  (bone parsedbf) initMemory
-  pure ()
+  loop
 
 initMemory :: Memory
 initMemory = Memory (repeat (0::Word8)) 0 (repeat (0::Word8))
+
+loop :: IO ()
+loop = do
+  putStr "haskell-BrainFk>"
+  hFlush stdout
+  bf <- getLine
+  let parsedbf = parseBrainFk $ pack bf
+  runM (bone parsedbf) initMemory
+  loop
+
 
