b528952b1af584405cad67328997040f21dbb6f4
yuki2501 <yuki2501@protonmail.com>
2021-03-02 15:51:25 +0900

    [change] change parse methods(use many1).

--- src/Parser.hs
+++ src/Parser.hs
@@ -7,55 +7,55 @@ import Data.Text
 import Data.Word
 
 data BrainFk a  where
-  BRight ::  BrainFk() -> BrainFk ()
-  BLeft  ::  BrainFk() -> BrainFk ()
-  Increment ::  BrainFk() -> BrainFk ()
-  Decrement ::   BrainFk() ->BrainFk ()
-  BPrint :: BrainFk() ->BrainFk ()
-  BGet ::   BrainFk() -> BrainFk ()
-  BLoop ::  Text -> BrainFk() -> BrainFk ()
-  BInvalidChar :: BrainFk () -> BrainFk()
+  BRight ::   BrainFk () 
+  BLeft  ::   BrainFk ()
+  Increment ::  BrainFk ()
+  Decrement ::   BrainFk ()
+  BPrint :: BrainFk ()
+  BGet ::    BrainFk ()
+  BLoop ::  Text -> BrainFk () 
+  BInvalidChar ::  BrainFk ()
   BNil ::  BrainFk ()
-  BError :: BrainFk()
-instance Show (BrainFk r) where
-  show (BRight r) = "BRight " ++ show r
-  show (BLeft r) = "BLeft " ++ show r
-  show (Increment r) = "Increment " ++ show r
-  show (Decrement r) = "Decrement" ++ show r
-  show (BPrint r) = "BPrint " ++ show r
-  show (BGet r) = "BGet " ++ show r
-  show (BLoop r r') = "BLoop " ++ show r ++ show r'
-  show BNil= "BNil" 
-  show (BInvalidChar r) = "BInvaildChar " ++ show r 
+  BError :: BrainFk ()
+instance Show (BrainFk a) where
+  show (BRight ) = "BRight " 
+  show (BLeft ) = "BLeft " 
+  show (Increment ) = "Increment " 
+  show (Decrement ) = "Decrement" 
+  show (BPrint ) = "BPrint " 
+  show (BGet ) = "BGet " 
+  show (BLoop r ) = "BLoop " 
+  show BNil = "BNil" 
+  show (BInvalidChar ) = "BInvaildChar " 
   show BError = "BError"
-parseBrainFk ::   Text -> BrainFk ()
+parseBrainFk ::   Text -> [BrainFk ()]
 parseBrainFk s = let result = showParseResult $ parse (brainfkParser <* endOfInput) s `feed` pack ""
                   in case result of
                        Right x -> x
-                       Left _ -> BError
+                       Left _ -> [BError]
 
 
-brainfkParser :: Parser (BrainFk ())
-brainfkParser = bright <|> bleft <|> increment <|> decrement <|> bprint <|> bget <|> bloop <|> binvaildchar <|> pure BNil
+brainfkParser :: Parser [BrainFk() ]
+brainfkParser =  many1 brainfk
   where
-    brainfk :: Parser (BrainFk ())
-    brainfk = bright <|> bleft <|> increment <|> decrement <|> bprint <|> bget<|> bloop <|> binvaildchar <|>   pure BNil
+    brainfk :: Parser (BrainFk() )
+    brainfk = bright <|> bleft <|> increment <|> decrement <|> bprint <|> bget<|> bloop <|> binvaildchar
     bright :: Parser (BrainFk ())
-    bright = BRight <$ char '>' <*> brainfk
+    bright = BRight <$ char '>' 
     bleft :: Parser (BrainFk ())
-    bleft = BLeft <$ char '<' <*> brainfk
+    bleft = BLeft <$ char '<' 
     increment :: Parser (BrainFk ())
-    increment = Increment <$ char '+' <*> brainfk
+    increment = Increment <$ char '+' 
     decrement :: Parser (BrainFk ())
-    decrement = Decrement <$ char '-' <*> brainfk
+    decrement = Decrement <$ char '-' 
     bprint :: Parser (BrainFk ())
-    bprint = BPrint <$ char '.' <*> brainfk
+    bprint = BPrint <$ char '.' 
     bget :: Parser (BrainFk ())
-    bget = BGet <$ char ',' <*> brainfk
+    bget = BGet <$ char ',' 
     bloop :: Parser (BrainFk ())
-    bloop = BLoop <$ char '[' <*> (pack <$> many1 (char '<' <|> char '>' <|> char '+' <|> char '-' <|> char '.' <|> char ',')) <* char ']' <*> brainfk
+    bloop = BLoop <$ char '[' <*> (pack <$> many1 (char '<' <|> char '>' <|> char '+' <|> char '-' <|> char '.' <|> char ',')) <* char ']' 
     binvaildchar :: Parser (BrainFk ())
-    binvaildchar = BInvalidChar <$ anyChar <*> brainfk
+    binvaildchar = BInvalidChar <$ anyChar 
 --(pack <$> many1 (char '<' <|> char '>' <|> char '+' <|> char '-' <|> char '.' <|> char ','))
 showParseResult :: Show a => Result a -> Either Text a
 showParseResult (Done _ r) = Right r
